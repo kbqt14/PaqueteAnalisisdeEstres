5f0c64128a91201eb53a2581fa1812913f280dc9
kbqt14 <kbqt14@gmail.com>
2018-08-30 00:16:51 -0300

    Primeros cambios guardados

---
+++ R/PaqueteCeibo.R
@@ -0,0 +1,77 @@
+Tabla5=function(x){
+  install.packages("dplyr")
+  require(dplyr)
+  DatosCeibo <-read.delim("Data/DatosCeibo.txt")
+  Sem5 <- filter(DatosCeibo, Semana== "5")
+  Sem5 %>%  group_by(Tratamiento) %>%
+    summarise_at(vars(-Tratamiento), funs(mean(., na.rm=TRUE)))
+
+
+
+
+}
+
+
+Tabla4=function(x){
+  install.packages("dplyr")
+  require(dplyr)
+  DatosCeibo <-read.delim("Data/DatosCeibo.txt")
+  Sem5 <- filter(DatosCeibo, Semana== "4")
+  Sem5 %>%  group_by(Tratamiento) %>%
+    summarise_at(vars(-Tratamiento), funs(mean(., na.rm=TRUE)))
+
+
+
+
+}
+
+Tabla4=function(x){
+  install.packages("dplyr")
+  require(dplyr)
+  DatosCeibo <-read.delim("Data/DatosCeibo.txt")
+  Sem5 <- filter(DatosCeibo, Semana== "5")
+  Sem5 %>%  group_by(Tratamiento) %>%
+    summarise_at(vars(-Tratamiento), funs(mean(., na.rm=TRUE)))
+
+
+
+
+}
+
+Tabla3=function(x){
+  install.packages("dplyr")
+  require(dplyr)
+  DatosCeibo <-read.delim("Data/DatosCeibo.txt")
+  Sem5 <- filter(DatosCeibo, Semana== "3")
+  Sem5 %>%  group_by(Tratamiento) %>%
+    summarise_at(vars(-Tratamiento), funs(mean(., na.rm=TRUE)))
+
+
+
+
+}
+
+Tabla2=function(x){
+  install.packages("dplyr")
+  require(dplyr)
+  DatosCeibo <-read.delim("Data/DatosCeibo.txt")
+  Sem5 <- filter(DatosCeibo, Semana== "2")
+  Sem5 %>%  group_by(Tratamiento) %>%
+    summarise_at(vars(-Tratamiento), funs(mean(., na.rm=TRUE)))
+
+
+
+
+}
+Tabla1=function(x){
+  install.packages("dplyr")
+  require(dplyr)
+  DatosCeibo <-read.delim("Data/DatosCeibo.txt")
+  Sem5 <- filter(DatosCeibo, Semana== "1")
+  Sem5 %>%  group_by(Tratamiento) %>%
+    summarise_at(vars(-Tratamiento), funs(mean(., na.rm=TRUE)))
+
+
+
+
+}
